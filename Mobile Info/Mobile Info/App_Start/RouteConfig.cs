4f3102266450cee4b27fc04ed7ca0d25574738b1
HUNIA_FATIMA <huniafatima99@gmail.com>
2019-04-22 22:16:44 +0500

    Front-end of Viewers

---
+++ Mobile Info/Mobile Info/App_Start/RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Mobile_Info
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "product",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Details", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Mobile Info/Mobile Info/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mobile_Info.Startup))]
+namespace Mobile_Info
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
